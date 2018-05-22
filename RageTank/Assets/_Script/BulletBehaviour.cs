94e6430a4dd42fad85fa6d80bc2e37247c0c3937
rohit.tolety <32849841+phantom7knight@users.noreply.github.com>
2018-05-21 21:49:35 -0700

    Made bullet need to implement the behaviour

---
+++ RageTank/Assets/_Script/BulletBehaviour.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletBehaviour : MonoBehaviour {
+
+    public GameObject player;
+    public GameObject bullet;
+
+	// Use this for initialization
+	void Start () {
+
+        player = GameObject.FindGameObjectWithTag("Player");
+        bullet = player.transform.GetChild(2).gameObject;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//bullet.transform
+	}
+}
