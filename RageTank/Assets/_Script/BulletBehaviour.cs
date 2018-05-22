3aae4893b9841fa55493eb45faabb2708aa5d098
rohit.tolety <32849841+phantom7knight@users.noreply.github.com>
2018-05-22 12:23:08 -0700

    Bullet bug need fixing

--- RageTank/Assets/_Script/BulletBehaviour.cs
+++ RageTank/Assets/_Script/BulletBehaviour.cs
@@ -7,16 +7,42 @@ public class BulletBehaviour : MonoBehaviour {
     public GameObject player;
     public GameObject bullet;
 
+    public float firerate = 10.0f;
+    public int bullet_count ;
+    public float damage = 10.0f;
+
+    public List<GameObject> bullets;
+   
+
 	// Use this for initialization
 	void Start () {
 
         player = GameObject.FindGameObjectWithTag("Player");
         bullet = player.transform.GetChild(2).gameObject;
 
+        //for(int i = 0; i < bullet_count; ++i )
+        //{
+        //    bullets = new List<GameObject>();
+        //}
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		//bullet.transform
+		if(Input.GetKeyDown(KeyCode.Space))
+        {
+            //shoot();
+            // damage -= 10.0f;
+            
+            transform.Translate(0, 100 * Time.deltaTime, 0);
+        }
+
 	}
+
+
+    void shoot()
+    {
+
+    }
+
 }
