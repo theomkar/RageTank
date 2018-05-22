b6f9bfa4ba2ee8f94b02bde4fd65d082a6d85c51
theOmkar <35416835+theomkar@users.noreply.github.com>
2018-05-21 21:41:38 -0700

    improved tank movement and planet position

--- RageTank/Assets/_Script/PlayerController.cs
+++ RageTank/Assets/_Script/PlayerController.cs
@@ -4,22 +4,40 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
-    public float movementSpeed = 10.0f;
+    public float movementSpeed = 20.0f;
+    public float turnRate = 5.0f;
     private Vector3 moveDir;
     Rigidbody m_rigidbody;
+
+    Transform m_transform;
 
     private void Start()
     {
         m_rigidbody = GetComponent<Rigidbody>();
+        
     }
 
     // Update is called once per frame
     void Update () {
-        moveDir = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxis("Vertical")).normalized;
-	}
+        moveDir = new Vector3(0, 0, Input.GetAxis("Vertical")).normalized;
+        m_transform = GetComponent<Transform>();
+
+        if (Input.GetKey("a"))
+        {
+            Debug.Log("a pressed");
+            this.transform.Rotate(0, Time.deltaTime - turnRate, 0, Space.World);
+        }
+
+        if (Input.GetKey("d"))
+        {
+            Debug.Log("a pressed");
+            this.transform.Rotate(0, Time.deltaTime + turnRate, 0, Space.World);
+        }
+    }
 
     private void FixedUpdate()
     {
-        m_rigidbody.MovePosition(m_rigidbody.position + transform.TransformDirection(moveDir) * movementSpeed * Time.deltaTime);   
+        m_rigidbody.MovePosition(m_rigidbody.position + transform.TransformDirection(moveDir) * movementSpeed * Time.deltaTime);
+     
     }
 }
